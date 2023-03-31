c0fc3c7865c031a759868c5bc30a19d34cf33bea
Peeyush Varyani <varyani.peeyush123@gmail.com>
2023-04-01 01:08:46 +0530

    first commit

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 let currentDrag = null;
 const base1 = document.getElementsByClassName("base1")[0];
 const base2 = document.getElementsByClassName("base2")[0];
+const topping1 = document.getElementsByClassName("topping1")[0];
+const topping2 = document.getElementsByClassName("topping2")[0];
+const topping3 = document.getElementsByClassName("topping3")[0];
+const topping1_img = document.getElementById("topping1")
+const topping2_img = document.getElementById("topping2")
+const topping3_img = document.getElementById("topping3")
 const pizzaArea = document.getElementsByClassName("pizza-show")[0];
 const baseSelect = document.getElementById("base-select");
 const selectTopping = document.getElementsByClassName("select-topping")[0];
 const selectBase = document.getElementsByClassName("select-base")[0];
 
+const showToppings = ()=>{
+    selectBase.style.display = "none";
+    selectTopping.style.display = "block";
+}
+const checkout = ()=>{
+    window.location.href = "./thankyou.html";
+}
+
 base1.addEventListener("dragstart", ()=>{
     currentDrag = "base1";
 });
 base1.addEventListener("dragend", ()=>{
-    currentDrag = null;
-
+    currentDrag = null; 
 });
 
 base2.addEventListener("dragstart", ()=>{
@@ -20,14 +33,47 @@ base2.addEventListener("dragstart", ()=>{
 base2.addEventListener("dragend", ()=>{
     currentDrag = null;
 });
+topping1.addEventListener("dragstart", ()=>{
+    currentDrag = "topping1";
+});
+topping1.addEventListener("dragend", ()=>{
+    currentDrag = null;
+});
+topping2.addEventListener("dragstart", ()=>{
+    currentDrag = "topping2";
+});
+topping2.addEventListener("dragend", ()=>{
+    currentDrag = null;
+});
+topping3.addEventListener("dragstart", ()=>{
+    currentDrag = "topping3";
+});
+topping3.addEventListener("dragend", ()=>{
+    currentDrag = null;
+});
 
 pizzaArea.addEventListener("dragover",(e)=>{
     e.preventDefault();
 })
 pizzaArea.addEventListener("drop",(e)=>{
     e.preventDefault();
-    if(currentDrag==="base1")
+    console.log(currentDrag)
+    if(currentDrag==="base1"){
         baseSelect.src = "./img/select-base1.png";
-    else if(currentDrag==="base2")
+        showToppings();
+    }
+    else if(currentDrag==="base2"){
         baseSelect.src = "./img/select-base2.png";
+        showToppings();
+    }
+    else if(currentDrag==="topping1"){
+        topping1_img.style.display="block";
+    }
+    else if(currentDrag==="topping2"){
+        topping2_img.style.display="block";
+    }
+    else if(currentDrag==="topping3"){
+        topping3_img.style.display="block";
+    }
+
 })
